c95d0103b01157c0b0f7a8ca2f98399d56cf5033
Hatem Gad <36468401+hatemsayed98@users.noreply.github.com>
2023-06-24 01:33:17 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangorestbussiness@gmail.com'
 EMAIL_HOST_PASSWORD = 'dazmpwtrpkxsfaty'
-# EMAIL_HOST_PASSWORD = 'djangorest'
 
 EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False
